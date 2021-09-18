72da9f84e87b70f92f33b7ff3dda52b5ab9e8aec
Marco Lanza <mlanzamolina@gmail.com>
2021-09-18 09:01:33 -0600

    cors handle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+const cors = require ('cors');
 const express = require('express');
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({extends: false}));
+app.use(cors());
 
 //definir routes
 app.use(require('./routes/index'));
