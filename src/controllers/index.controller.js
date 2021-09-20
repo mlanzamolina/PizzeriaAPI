2fcfc950c9ecd8db8fd9c9a6cd362b2948881ad9
rodrigo bardales <rodrigo.bardales@hotmail.com>
2021-09-19 22:17:21 -0600

    getUserporusername

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,197 +1,222 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
-const pool = new Pool ({
-    host: 'b5hctfvcrt6ruttschyi-postgresql.services.clever-cloud.com',
-    user: 'u79mr35fsvyg7hr5yill',
-    password: 'gaCrbxaYmhkBKQYBy6nZ',
-    database: 'b5hctfvcrt6ruttschyi',
-    port: '5432'
-})
+const pool = new Pool({
+  host: "b5hctfvcrt6ruttschyi-postgresql.services.clever-cloud.com",
+  user: "u79mr35fsvyg7hr5yill",
+  password: "gaCrbxaYmhkBKQYBy6nZ",
+  database: "b5hctfvcrt6ruttschyi",
+  port: "5432",
+});
 
 //USUARIOS
 const getUsers = async (req, res) => {
-    const respone = await pool.query('select * from usuario');
-    res.json(respone.rows)
-}
+  const respone = await pool.query("select * from usuario");
+  res.json(respone.rows);
+};
 
 const getUserByID = async (req, res) => {
-    const id = req.params.user_id;
-    const response = await pool.query('SELECT * FROM usuario Where user_id = $1', [id]);
-    res.json(response.rows);
-}
+  const id = req.params.user_id;
+  const response = await pool.query(
+    "SELECT * FROM usuario Where user_id = $1",
+    [id]
+  );
+  res.json(response.rows);
+};
+
+const getUserByUsername = async (req, res) => {
+  const username = req.params.username;
+  const response = await pool.query(
+    "SELECT * FROM usuario Where username = $1",
+    [username]
+  );
+  res.json(response.rows);
+};
 
 const createUser = async (req, res) => {
-    const {nombre, clave, admin, apellido, username} = req.body;
-    const resonse = await pool.query(`insert into usuario (nombre, clave, admin, apellido, username) 
+  const { nombre, clave, admin, apellido, username } = req.body;
+  const resonse = await pool.query(
+    `insert into usuario (nombre, clave, admin, apellido, username) 
         values ($1, $2, $3, $4, $5)`,
-        [nombre, clave, admin, apellido, username])
-    console.log(resonse);
-    res.send('user created');
-}
-
+    [nombre, clave, admin, apellido, username]
+  );
+  console.log(resonse);
+  res.send("user created");
+};
 
 //PRODUCTOS
 const getProducto = async (req, res) => {
-    const respone = await pool.query('select * from producto');
-    res.json(respone.rows)
-}
+  const respone = await pool.query("select * from producto");
+  res.json(respone.rows);
+};
 
 const getProductoByID = async (req, res) => {
-    const id = req.params.idproducto;
-    var querty = 'SELECT * FROM producto Where idproducto = $1';
-    const response = await pool.query(querty, [id]);
-    res.json(response.rows);
-}
+  const id = req.params.idproducto;
+  var querty = "SELECT * FROM producto Where idproducto = $1";
+  const response = await pool.query(querty, [id]);
+  res.json(response.rows);
+};
 
 const createProducto = async (req, res) => {
-    const {nombre_producto, descripcion, precio, imagen, active} = req.body;
-    const response = await pool.query('insert into producto (nombre_producto, descripcion, precio, imagen, active) values ($1, $2, $3, $4, $5)',
-         [nombre_producto, descripcion, precio, imagen, active])
-    console.log(response);
-    res.send('user created');
-}
+  const { nombre_producto, descripcion, precio, imagen, active } = req.body;
+  const response = await pool.query(
+    "insert into producto (nombre_producto, descripcion, precio, imagen, active) values ($1, $2, $3, $4, $5)",
+    [nombre_producto, descripcion, precio, imagen, active]
+  );
+  console.log(response);
+  res.send("user created");
+};
 
 const deleteProducto = async (req, res) => {
-    const id = req.params.idproducto;
-    const response = await pool.query('DELETE FROM producto WHERE idproducto = $1', [id]);
-    console.log(response);
-    res.json('User ${idproducto} deleted successfully');
-}
+  const id = req.params.idproducto;
+  const response = await pool.query(
+    "DELETE FROM producto WHERE idproducto = $1",
+    [id]
+  );
+  console.log(response);
+  res.json("User ${idproducto} deleted successfully");
+};
 
 const updateProducto = async (req, res) => {
-    const idproducto = req.params.idproducto;
-    const {precio, active} = req.body;
-    const response = await pool.query('UPDATE producto SET precio = $1, active = $2 WHERE idproducto = $3',
-    [
-        precio,
-        active,
-        idproducto
-    ]);
-    console.log(response);
-    res.json('producto actualizado');
-}
+  const idproducto = req.params.idproducto;
+  const { precio, active } = req.body;
+  const response = await pool.query(
+    "UPDATE producto SET precio = $1, active = $2 WHERE idproducto = $3",
+    [precio, active, idproducto]
+  );
+  console.log(response);
+  res.json("producto actualizado");
+};
 
 //REVIEWS
 const getReviews = async (req, res) => {
-    const respone = await pool.query('select * from review');
-    res.json(respone.rows)
-}
+  const respone = await pool.query("select * from review");
+  res.json(respone.rows);
+};
 
 const createReview = async (req, res) => {
-    const { idproducto, review } = req.body;
-    const response = await pool.query('insert into review (idproducto, review) values ($1, $2)',
-         [idproducto, review])
-    console.log(response);
-    res.send('review created');
-}
+  const { idproducto, review } = req.body;
+  const response = await pool.query(
+    "insert into review (idproducto, review) values ($1, $2)",
+    [idproducto, review]
+  );
+  console.log(response);
+  res.send("review created");
+};
 
 const getReviewsByProduct = async (req, res) => {
-    const id = req.params.idproducto;
-    var querty = 'SELECT * FROM review Where idproducto = $1';
-    const response = await pool.query(querty, [id]);
-    res.json(response.rows);
-}
+  const id = req.params.idproducto;
+  var querty = "SELECT * FROM review Where idproducto = $1";
+  const response = await pool.query(querty, [id]);
+  res.json(response.rows);
+};
 
 //Orden
 const getOrdenes = async (req, res) => {
-    const respone = await pool.query('select * from orden');
-    res.json(respone.rows)
-}
+  const respone = await pool.query("select * from orden");
+  res.json(respone.rows);
+};
 
 const createOrden = async (req, res) => {
-    const { aprovado , user_id, ordertotal } = req.body;
-    const response = await pool.query('insert into orden (aprovado , user_id, ordertotal) values ($1, $2, $3)',
-         [aprovado , user_id, ordertotal])
-    console.log(response);
-    res.send('orden created');
-}
+  const { aprovado, user_id, ordertotal } = req.body;
+  const response = await pool.query(
+    "insert into orden (aprovado , user_id, ordertotal) values ($1, $2, $3)",
+    [aprovado, user_id, ordertotal]
+  );
+  console.log(response);
+  res.send("orden created");
+};
 
 const updateOrden = async (req, res) => {
-    const order_id = req.params.order_id;
-    const {aprovado, ordertotal} = req.body;
-    const response = await pool.query('UPDATE orden SET aprovado = $1, ordertotal = $2 WHERE order_id = $3',
-    [
-        aprovado,
-        ordertotal,
-        order_id
-    ]);
-    console.log(response);
-    res.json('producto actualizado');
-}
+  const order_id = req.params.order_id;
+  const { aprovado, ordertotal } = req.body;
+  const response = await pool.query(
+    "UPDATE orden SET aprovado = $1, ordertotal = $2 WHERE order_id = $3",
+    [aprovado, ordertotal, order_id]
+  );
+  console.log(response);
+  res.json("producto actualizado");
+};
 
 const getOrdenByID = async (req, res) => {
-    const id = req.params.order_id;
-    const response = await pool.query('SELECT * FROM orden WHERE order_id = $1', [id]);
-    res.json(response.rows);
-}
+  const id = req.params.order_id;
+  const response = await pool.query("SELECT * FROM orden WHERE order_id = $1", [
+    id,
+  ]);
+  res.json(response.rows);
+};
 
 //DETALLE_ORDEN
 const getDetalle_Orden = async (req, res) => {
-    const response = await pool.query('SELECT * FROM detalle_orden');
-    res.json(response.rows);
-}
+  const response = await pool.query("SELECT * FROM detalle_orden");
+  res.json(response.rows);
+};
 
-const createDetalle_Orden = async ( req, res ) => {
-    const { idproducto , order_id, cantidad } = req.body;
-    const response = await pool.query('insert into detalle_orden (idproducto , order_id, cantidad) values ($1, $2, $3)',
-         [idproducto , order_id, cantidad])
-    console.log(response);
-    res.send('orden created');
-}
+const createDetalle_Orden = async (req, res) => {
+  const { idproducto, order_id, cantidad } = req.body;
+  const response = await pool.query(
+    "insert into detalle_orden (idproducto , order_id, cantidad) values ($1, $2, $3)",
+    [idproducto, order_id, cantidad]
+  );
+  console.log(response);
+  res.send("orden created");
+};
 
-const getDetalleByID = async (req, res) => {
-    const id = req.params.orderdetail_id;
-    const response = await pool.query('SELECT * FROM detalle_orden WHERE orderdetail_id = $1', [id]);
-    res.json(response.rows);
-}
+// const getDetalleByID = async (req, res) => {
+//     const id = req.params.orderdetail_id;
+//     const response = await pool.query('SELECT * FROM detalle_orden WHERE orderdetail_id = $1', [id]);
+//     res.json(response.rows);
+// }
 
 const getDetalleByOrderID = async (req, res) => {
-    const id = req.params.order_id;
-    const response = await pool.query('SELECT * FROM detalle_orden WHERE order_id = $1', [id]);
-    res.json(response.rows);
-}
+  const id = req.params.order_id;
+  const response = await pool.query(
+    "SELECT * FROM detalle_orden WHERE order_id = $1",
+    [id]
+  );
+  res.json(response.rows);
+};
 
-//no funca
-const updateDetalle = async ( req, res ) => {
-    const orderdetail_id = req.params.orderdetail_id;
-    console.log(orderdetail_id);
-    const cantidad = req.body;
-    const response = await pool.query('UPDATE detalle_orden SET cantidad = $1 WHERE orderdetail_id = $2',
-    [
-        cantidad,
-        orderdetail_id
-    ]);
-    console.log(response);
-    res.json('detalle actualizado');
-}
+
+const updateDetalle = async (req, res) => {
+  const orderdetail_id = req.params.orderdetail_id;
+  const { cantidad } = req.body;
+  const response = await pool.query(
+    "UPDATE detalle_orden SET cantidad = $1 WHERE orderdetail_id = $2",
+    [cantidad, orderdetail_id]
+  );
+  console.log(response);
+  res.json("producto actualizado");
+};
 
 const deleteDetalle = async (req, res) => {
-    const id = req.params.orderdetail_id;
-    const response = await pool.query('DELETE FROM detalle_orden WHERE orderdetail_id = $1', [id]);
-    console.log(response);
-    res.json('Detail ${id} deleted successfully');
-}
+  const orderdetail_id = req.params.orderdetail_id;
+  const response = await pool.query('DELETE FROM detalle_orden WHERE orderdetail_id = $1', [orderdetail_id]
+  );
+  console.log(response);
+  res.json("Detail ${id} deleted successfully");
+};
 
 module.exports = {
-    getUsers,
-    createUser,
-    getUserByID,
-    getProducto,
-    getProductoByID,
-    createProducto,
-    deleteProducto,
-    updateProducto,
-    getReviews,
-    createReview,
-    getReviewsByProduct,
-    getOrdenes,
-    createOrden,
-    updateOrden,
-    getOrdenByID,
-    getDetalle_Orden,
-    createDetalle_Orden,
-    getDetalleByID,
-    getDetalleByOrderID,
-    updateDetalle,
-    deleteDetalle
-}
+  getUsers,
+  createUser,
+  getUserByID,
+  getProducto,
+  getProductoByID,
+  createProducto,
+  deleteProducto,
+  updateProducto,
+  getReviews,
+  createReview,
+  getReviewsByProduct,
+  getOrdenes,
+  createOrden,
+  updateOrden,
+  getOrdenByID,
+  getDetalle_Orden,
+  createDetalle_Orden,
+  // getDetalleByID,
+  getDetalleByOrderID,
+  updateDetalle,
+  deleteDetalle,
+  getUserByUsername
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 const { Router } =  require('express');
 const router = Router();
 
-const { getUsers, createUser, getUserByID } = require('../controllers/index.controller');
+const { getUsers, createUser, getUserByUsername, getUserByID } = require('../controllers/index.controller');
 const { getProducto, createProducto, getProductoByID, deleteProducto, updateProducto } = require('../controllers/index.controller');
 const { getReviews, createReview, getReviewsByProduct } = require('../controllers/index.controller');
 const { getOrdenes, createOrden, updateOrden, getOrdenByID } = require('../controllers/index.controller');
-const { getDetalle_Orden, createDetalle_Orden, getDetalleByID, getDetalleByOrderID, updateDetalle, deleteDetalle } = require('../controllers/index.controller');
+const { getDetalle_Orden, createDetalle_Orden, getDetalleByOrderID, updateDetalle, deleteDetalle } = require('../controllers/index.controller');
 
 //USUARIOS
 router.get('/usuarios', getUsers);
-router.get('/usuarios/:user_id', getUserByID);
+//router.get('/usuarios/:user_id', getUserByID);
+router.get('/usuarios/:username', getUserByUsername)
 router.post('/usuarios', createUser);
 
 //PRODUCTOS
@@ -32,10 +33,11 @@ router.put('/ordenes/:order_id', updateOrden);
 
 //DETALLE_ORDEN
 router.get('/detalles', getDetalle_Orden);
-router.get('/detalles/:orderdetail_id', getDetalleByID);
-router.get('/detalles/:orderdetail_id/:order_id', getDetalleByOrderID);
+// router.get('/detalles/:orderdetail_id', getDetalleByID);
+// router.get('/detalles/:orderdetail_id/:order_id', getDetalleByOrderID);
+router.get('/detalles/:order_id', getDetalleByOrderID);
 router.post('/detalles', createDetalle_Orden);
-router.put('/detalles:orderdetail_id', updateDetalle);
-router.delete('/detalles:orderdetail_id', deleteDetalle);
+router.put('/detalles/:orderdetail_id', updateDetalle);
+router.delete('/detalles/:orderdetail_id', deleteDetalle);
 
 module.exports = router;
